7c0fa4efdd2c28e0b0f1b96011ed7d4ccf99c63d
苏超 <suchao@sc12138.local>
2023-03-29 00:01:33 +0800

    修改eslint报错的问题

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
@@ -20,12 +20,10 @@ import { lightTheme } from './assets/theme';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    {/* <Suspense fallback="loading"> */}
     <ThemeProvider theme={lightTheme}>
       <HashRouter>
         <App />
       </HashRouter>
     </ThemeProvider>
-    {/* </Suspense> */}
   </Provider >
 );
